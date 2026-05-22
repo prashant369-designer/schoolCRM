6394e118b690bdeb7647ed2e2080457f33848d13
Prashant Chaudhary <104970128+prashant369-designer@users.noreply.github.com>
2026-05-22 12:42:49 +0530

    commit db files

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'schoolerm',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
